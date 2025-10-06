ee21eb4d12bac4748c16ee235ec5aa477637889b
Michael Mussato <michimussato@gmail.com>
2025-10-06 14:15:25 +0200

    - harbor cli

---
+++ README.md
@@ -0,0 +1,85 @@
+<!-- TOC -->
+* [OpenStudioLandscapesUtil-ReadmeGenerator](#openstudiolandscapesutil-readmegenerator)
+  * [Requirements](#requirements)
+    * [venv](#venv)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tagging](#tagging)
+    * [Release Candidata](#release-candidata)
+    * [Main Release](#main-release)
+<!-- TOC -->
+
+---
+
+# OpenStudioLandscapesUtil-HarborCLI
+
+The `openstudiolandscapesutil-harborcli` facilitates getting Harbor up and running for
+[OpenStudioLandscapes](https://github.com/michimussato/OpenStudioLandscapes).
+
+This package was created using PyScaffold:
+
+```shell
+putup --package Harbor_CLI --venv .venv --no-tox --license AGPL-3.0-or-later --force --namespace OpenStudioLandscapesUtil OpenStudioLandscapesUtil-HarborCLI
+```
+
+## Requirements
+
+- `python3.11`
+
+### venv
+
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+## Installation
+
+`pip install git+https://github.com/michimussato/OpenStudioLandscapesUtil-HarborCLI.git`
+
+## Usage
+
+```
+$ openstudiolandscapesutil-harborcli --help
+Usage: openstudiolandscapesutil-harborcli [OPTIONS] COMMAND [ARGS]...
+
+  A Harbor CLI entrypoint.
+
+Options:
+  -v, --verbosity TEXT  Set verbosity of output: CRITICAL, FATAL, ERROR, WARN,
+                        WARNING, INFO, DEBUG, NOTSET
+  --help                Show this message and exit.
+
+Commands:
+  configure          Step 3
+  download           Step 1
+  extract            Step 2
+  prepare            Step 4
+  systemd-install    Step 5
+  systemd-uninstall
+```
+
+## Tagging
+
+### Release Candidate
+
+```shell
+NEW_TAG=X.X.X-rcX
+```
+
+```shell
+git tag --annotate "v${NEW_TAG}" --message "Release Candidate Version v${NEW_TAG}" --force
+git push --tags --force
+```
+
+### Main Release
+
+```shell
+NEW_TAG=X.X.X
+```
+
+```shell
+git tag --annotate "v${NEW_TAG}" --message "Main Release Version v${NEW_TAG}" --force
+git tag --annotate "latest" --message "Latest Release Version (pointing to v${NEW_TAG})" v${NEW_TAG}^{} --force
+git push --tags --force
+```
---
+++ src/OpenStudioLandscapesUtil/Harbor_CLI/harbor_cli.py
@@ -0,0 +1,692 @@
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+``[options.entry_points]`` section in ``setup.cfg``::
+
+    console_scripts =
+         fibonacci = Harbor_CLI.skeleton:run
+
+Then run ``pip install .`` (or ``pip install -e .`` for editable mode)
+which will install the command ``fibonacci`` inside your current environment.
+
+Besides console scripts, the header (i.e. until ``_logger``...) of this file can
+also be used as template for Python modules.
+
+Note:
+    This file can be renamed depending on your needs or safely removed if not needed.
+
+References:
+    - https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+    - https://pip.pypa.io/en/stable/reference/pip_install
+"""
+import configparser
+import os
+import pathlib
+import shutil
+import subprocess
+import tarfile
+import typing
+from subprocess import CompletedProcess
+from typing import Union, Any
+
+import click
+import requests
+import logging
+import sys
+
+import yaml
+from OpenStudioLandscapesUtil.Harbor_CLI import __version__
+
+__author__ = "Michael Mussato"
+__copyright__ = "Michael Mussato"
+__license__ = "AGPL-3.0-or-later"
+
+_logger = logging.getLogger(__name__)
+
+
+# DEFAULTS
+
+SHELL = [
+    shutil.which("bash"),
+    "-c",
+]
+_SU_METHODS= {
+    "su": [
+        shutil.which("su"),
+        "-",
+        "root",
+    ],
+    "sudo": [
+        shutil.which("sudo"),
+        "--user=root",
+    ],
+    "pkexec": [
+        shutil.which("pkexec"),
+    ],
+}
+
+SU_METHOD = _SU_METHODS["pkexec"]
+
+HARBOR_URL: str = "https://github.com/goharbor/harbor/releases/download/v2.12.2/harbor-online-installer-v2.12.2.tgz"
+HARBOR_ROOT_DIR: pathlib.Path = pathlib.Path("/home/michael/git/repos/OpenStudioLandscapes/.harbor3")
+HARBOR_DOWNLOAD_DIR: pathlib.Path = HARBOR_ROOT_DIR.joinpath("download")
+HARBOR_BIN_DIR: pathlib.Path = HARBOR_ROOT_DIR.joinpath("bin")
+HARBOR_DATA_DIR: pathlib.Path = HARBOR_ROOT_DIR.joinpath("data")
+HARBOR_CONFIG_ROOT: pathlib.Path = HARBOR_BIN_DIR
+HARBOR_PREPARE: pathlib.Path = HARBOR_BIN_DIR.joinpath("prepare")
+
+OPENSTUDIOLANDSCAPES__DOMAIN_LAN: str = "farm.evil"
+OPENSTUDIOLANDSCAPES__HARBOR_HOSTNAME: str = "harbor.{OPENSTUDIOLANDSCAPES__DOMAIN_LAN}".format(OPENSTUDIOLANDSCAPES__DOMAIN_LAN=OPENSTUDIOLANDSCAPES__DOMAIN_LAN)
+OPENSTUDIOLANDSCAPES__HARBOR_PORT: int = 80
+OPENSTUDIOLANDSCAPES__HARBOR_PASSWORD: str = "Harbor12345"
+
+HARBOR_CONFIG_DICT: dict = {
+    "hostname": OPENSTUDIOLANDSCAPES__HARBOR_HOSTNAME,
+    "http": {"port": OPENSTUDIOLANDSCAPES__HARBOR_PORT},
+    "harbor_admin_password": OPENSTUDIOLANDSCAPES__HARBOR_PASSWORD,
+    "database": {
+        "password": "root123",
+        "max_idle_conns": 100,
+        "max_open_conns": 900,
+        "conn_max_idle_time": 0,
+    },
+    "data_volume": HARBOR_DATA_DIR.as_posix(),
+    "trivy": {
+        "ignore_unfixed": False,
+        "skip_update": False,
+        "skip_java_db_update": False,
+        "offline_scan": False,
+        "security_check": "vuln",
+        "insecure": False,
+        "timeout": "5m0s",
+    },
+    "jobservice": {
+        "max_job_workers": 10,
+        "job_loggers": ["STD_OUTPUT", "FILE"],
+        "logger_sweeper_duration": 1,
+    },
+    "notification": {
+        "webhook_job_max_retry": 3,
+        "webhook_job_http_client_timeout": 3,
+    },
+    "log": {
+        "level": "info",
+        "local": {
+            "rotate_count": 50,
+            "rotate_size": "200M",
+            "location": "/var/log/harbor",
+        },
+    },
+    "_version": "2.12.0",
+    "proxy": {
+        "http_proxy": None,
+        "https_proxy": None,
+        "no_proxy": None,
+        "components": ["core", "jobservice", "trivy"],
+    },
+    "upload_purging": {
+        "enabled": True,
+        "age": "168h",
+        "interval": "24h",
+        "dryrun": False,
+    },
+    "cache": {"enabled": False, "expire_hours": 24},
+}
+
+SYSTEMD_UNIT: pathlib.Path = pathlib.Path("/usr/lib/systemd/system/harbor.service")
+
+DOCKER_PROGRESS = [
+    "auto",
+    "quiet",
+    "plain",
+    "tty",
+    "rawjson",
+]
+
+
+# CLI
+
+@click.group()
+@click.option(
+    "--verbosity",
+    "-v",
+    is_flag=False,
+    default="INFO",
+    help=f"Set verbosity of output: {', '.join(logging.getLevelNamesMapping().keys())}",
+)
+def run(verbosity):
+    """A Harbor CLI entrypoint."""
+
+    level = logging.getLevelName(verbosity)
+
+    if verbosity not in logging.getLevelNamesMapping().keys():
+        raise ValueError("Invalid verbosity level: {}".format(verbosity))
+
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=level, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    _logger.debug(logging.getLevelName(_logger.getEffectiveLevel()))
+
+    _logger.info("Logging setup complete")
+
+
+@run.command()
+@click.option(
+    "--url",
+    type=click.STRING,
+    default=HARBOR_URL,
+    show_default=True,
+    help="URL of the Harbor installer",
+    required=True,
+    prompt=True,
+)
+@click.option(
+    "--destination-folder",
+    type=pathlib.Path,
+    default=HARBOR_DOWNLOAD_DIR,
+    show_default=True,
+    help="URL of the Harbor installer",
+    required=True,
+    prompt=True,
+)
+def download(
+        url: str,
+        destination_folder: pathlib.Path,
+) -> Union[pathlib.Path, Exception]:
+    """Step 1"""
+
+    _logger.debug(url)
+    _logger.debug(destination_folder)
+
+    destination_folder.mkdir(parents=True, exist_ok=True)
+
+    if not destination_folder.exists():
+        destination_folder.mkdir(
+            parents=True, exist_ok=True
+        )  # create folder if it does not exist
+
+    tar_filename = url.split("/")[-1].replace(" ", "_")  # be careful with file names
+    tar_file_path = destination_folder / tar_filename
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        _logger.info("Saving to %s" % tar_file_path.absolute().as_posix())
+        with open(tar_file_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+
+        return tar_file_path
+
+    else:  # HTTP status code 4XX/5XX
+        raise Exception(
+            "Download failed: status code {}\n{}".format(r.status_code, r.text)
+        )
+
+
+@run.command()
+@click.option(
+    "--extract-to",
+    type=pathlib.Path,
+    default=HARBOR_BIN_DIR,
+    show_default=True,
+    help="Full Path of the destination folder",
+    required=True,
+    prompt=True,
+)
+@click.option(
+    "--tar-file",
+    type=pathlib.Path,
+    default=HARBOR_DOWNLOAD_DIR.joinpath("harbor-online-installer-v2.12.2.tgz"),
+    show_default=True,
+    help="Full Path to the Harbor TAR installer.",
+    required=True,
+    prompt=True,
+)
+def extract(
+        extract_to: pathlib.Path,
+        tar_file: pathlib.Path,
+) -> Union[pathlib.Path, Exception]:
+    """Step 2"""
+
+    _logger.debug(extract_to)
+    _logger.debug(tar_file)
+
+    harbor_bin_dir: pathlib.Path = extract_to
+    harbor_bin_dir.mkdir(parents=True, exist_ok=True)
+
+    # equivalent to tar --strip-components=1
+    # Credits: https://stackoverflow.com/a/78461535
+    strip1 = lambda member, path: member.replace(
+        name=pathlib.Path(*pathlib.Path(member.path).parts[1:])
+    )
+
+    _logger.debug("Extracting tar file...")
+    with tarfile.open(tar_file, "r:gz") as tar:
+        tar.extractall(
+            path=harbor_bin_dir,
+            filter=strip1,
+        )
+    _logger.debug("All files extracted to %s" % harbor_bin_dir.as_posix())
+
+    return extract_to
+
+
+def _configure() -> str:
+
+    harbor_yml: str = yaml.dump(
+        HARBOR_CONFIG_DICT,
+        indent=2,
+    )
+
+    return harbor_yml
+
+
+@run.command()
+@click.option(
+    "--out-dir",
+    type=pathlib.Path,
+    default=HARBOR_CONFIG_ROOT,
+    show_default=True,
+    help="Full Path where the harbor.yml will be saved.",
+    required=True,
+    prompt=True,
+)
+def configure(
+        out_dir: pathlib.Path,
+) -> Union[pathlib.Path, Exception]:
+    """Step 3"""
+
+    _logger.debug(out_dir)
+
+    harbor_yml: pathlib.Path = out_dir.joinpath("harbor.yml")
+    harbor_yml.parent.mkdir(parents=True, exist_ok=True)
+
+    harbor_yml_data: str = _configure()
+
+    with open(harbor_yml, "w") as fw:
+        fw.write(harbor_yml_data)
+
+    return harbor_yml
+
+
+@run.command()
+@click.option(
+    "--prepare-script",
+    type=pathlib.Path,
+    default=HARBOR_PREPARE,
+    show_default=True,
+    help="Full Path to the Harbor prepare script.",
+    required=True,
+    prompt=True,
+)
+def prepare(
+        prepare_script: pathlib.Path,
+) -> CompletedProcess[bytes]:
+    """Step 4"""
+
+    _logger.debug(prepare_script)
+
+    if not prepare_script.exists():
+        raise FileNotFoundError("`prepare` file not found. Not able to continue.")
+
+    if not HARBOR_CONFIG_ROOT.joinpath("harbor.yml").exists():
+        raise FileNotFoundError("`harbor.yml` file not found. Run `openstudiolandscapesutil-harborcli configure`.")
+
+    _logger.debug("Preparing Harbor...")
+
+    bash_c = [
+        # *su_method,
+        *SHELL,
+    ]
+
+    cmd_prepare = [
+        prepare_script.as_posix(),
+    ]
+
+    ret = subprocess.run(
+        f"{' '.join(bash_c)} \"{' '.join(cmd_prepare)}\"",
+        shell=True,
+        check=True,
+    )
+
+    return ret
+
+
+def _systemd_unit_dict(
+        working_directory: pathlib.Path,
+        exec_start: typing.List[str],
+        exec_reload: typing.List[str],
+        exec_stop: typing.List[str],
+) -> typing.Dict:
+
+    unit_dict = {
+        "Unit": {
+            "Description": "Harbor",
+            "Documentation": "https://goharbor.io/",
+        },
+        "Service": {
+            "Type": "simple",
+            "User": "root",
+            "Group": "root",
+            "Restart": "always",
+            "WorkingDirectory": working_directory.as_posix(),
+            "ExecStart": " ".join(exec_start),
+            "ExecReload": " ".join(exec_reload),
+            "ExecStop": " ".join(exec_stop),
+        },
+        "Install": {
+            "WantedBy": "multi-user.target",
+        },
+    }
+
+    _logger.debug(unit_dict)
+
+    return unit_dict
+
+
+@run.command()
+# @click.option(
+#     "--install",
+#     is_flag=True,
+#     # type=pathlib.Path,
+#     # default=HARBOR_PREPARE,
+#     # show_default=True,
+#     help="Install systemd unit.",
+#     required=True,
+#     # prompt=True,
+# )
+@click.option(
+    "--enable",
+    is_flag=True,
+    help="Enable systemd unit.",
+    required=False,
+)
+@click.option(
+    "--start",
+    is_flag=True,
+    help="Start systemd unit.",
+    required=False,
+)
+def systemd_install(
+        enable: bool,
+        start: bool,
+        # install: bool,
+) -> list[str | Any]:
+    """Step 5"""
+
+    _logger.debug(start)
+    _logger.debug(enable)
+
+    _cmd_harbor = [
+        shutil.which("docker"),
+        "compose",
+        "--progress",
+        DOCKER_PROGRESS[2],
+        "--file",
+        HARBOR_CONFIG_ROOT.joinpath("docker-compose.yml").as_posix(),
+        "--project-name",
+        "openstudiolandscapes-harbor",
+    ]
+
+    exec_start = [
+        *_cmd_harbor,
+        "up",
+        "--remove-orphans",
+    ]
+
+    exec_reload = [
+        *_cmd_harbor,
+        "restart",
+    ]
+
+    exec_stop = [
+        *_cmd_harbor,
+        "down",
+    ]
+
+    unit_dict: dict = _systemd_unit_dict(
+        working_directory=HARBOR_BIN_DIR,
+        exec_start=exec_start,
+        exec_reload=exec_reload,
+        exec_stop=exec_stop,
+    )
+
+    unit: configparser.ConfigParser = configparser.ConfigParser()
+    # Change from case insensitive to case sensitive
+    # https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.optionxform
+    unit.optionxform = str
+
+    unit.read_dict(unit_dict)
+
+    unit_file_tmp = HARBOR_BIN_DIR.joinpath(SYSTEMD_UNIT.name)
+
+    unit_file_tmp.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(unit_file_tmp, "w") as fw:
+        unit.write(fw, space_around_delimiters=False)
+
+    with open(unit_file_tmp, "r") as fr:
+        unit_file_content = fr.read()
+
+    _logger.debug(unit_file_content)
+
+    copy_service = [
+        shutil.which("cp"),
+        unit_file_tmp.as_posix(),
+        SYSTEMD_UNIT.as_posix(),
+    ]
+
+    set_permissions = [
+        shutil.which("chmod"),
+        "644",
+        SYSTEMD_UNIT.as_posix(),
+    ]
+
+    daemon_reload = [
+        shutil.which("systemctl"),
+        "daemon-reload",
+    ]
+
+    systemctl_start = [
+        shutil.which("systemctl"),
+        "start",
+        SYSTEMD_UNIT.name,
+    ]
+
+    systemctl_enable = [
+        shutil.which("systemctl"),
+        "enable",
+        SYSTEMD_UNIT.name,
+    ]
+
+    install_service = [
+        *copy_service,
+        "&&",
+        *set_permissions,
+        "&&",
+        *daemon_reload,
+    ]
+
+    if start:
+        install_service.extend(
+            [
+                "&&",
+                *systemctl_start,
+            ]
+        )
+
+    if enable:
+        install_service.extend(
+            [
+                "&&",
+                *systemctl_enable,
+            ]
+        )
+
+    _logger.debug(f"{install_service = }")
+
+    sudo_bash_c = [
+        *SU_METHOD,
+        *SHELL,
+    ]
+
+    cmd = [
+        *sudo_bash_c,
+        " ".join(install_service)
+    ]
+
+    # _logger.debug(f"{cmd = }")
+
+    # proc = subprocess.run(
+    #     [
+    #         *sudo_bash_c,
+    #         " ".join(install_service)
+    #     ],
+    #     shell=True,
+    #     check=True,
+    # )
+
+    _logger.info("Execute the following command manually:")
+    _logger.info(f"\n{' '.join(sudo_bash_c)} \"{' '.join(install_service)}\"")
+
+    return cmd
+
+
+@run.command()
+# @click.option(
+#     "--install",
+#     is_flag=True,
+#     # type=pathlib.Path,
+#     # default=HARBOR_PREPARE,
+#     # show_default=True,
+#     help="Install systemd unit.",
+#     required=True,
+#     # prompt=True,
+# )
+@click.option(
+    "--disable",
+    is_flag=True,
+    help="Disable systemd unit.",
+    required=False,
+)
+@click.option(
+    "--stop",
+    is_flag=True,
+    help="Stop systemd unit.",
+    required=True,
+)
+@click.option(
+    "--remove",
+    is_flag=True,
+    help="Remove systemd unit.",
+    required=False,
+)
+def systemd_uninstall(
+        disable: bool,
+        stop: bool,
+        remove: bool,
+) -> list[str | Any]:
+
+    if remove:
+        stop = True
+        disable = True
+
+    _logger.debug(stop)
+    _logger.debug(disable)
+    _logger.debug(remove)
+
+    systemctl_disable = [
+        shutil.which("systemctl"),
+        "disable",
+        SYSTEMD_UNIT.name,
+    ]
+
+    systemctl_stop = [
+        shutil.which("systemctl"),
+        "stop",
+        SYSTEMD_UNIT.name,
+    ]
+
+    remove_service = [
+        shutil.which("rm"),
+        SYSTEMD_UNIT.as_posix(),
+    ]
+
+    daemon_reload = [
+        shutil.which("systemctl"),
+        "daemon-reload",
+    ]
+
+    uninstall_service = [
+        *systemctl_stop,
+    ]
+
+    if disable:
+        uninstall_service.extend(
+            [
+                "&&",
+                *systemctl_disable
+            ]
+        )
+
+    if remove:
+        uninstall_service.extend(
+            [
+                "&&",
+                *remove_service
+            ]
+        )
+
+        uninstall_service.extend(
+            [
+                "&&",
+                *daemon_reload,
+            ]
+        )
+
+    _logger.debug(f"{uninstall_service = }")
+
+    sudo_bash_c = [
+        *SU_METHOD,
+        *SHELL,
+    ]
+
+    cmd = [
+        *sudo_bash_c,
+        " ".join(uninstall_service)
+    ]
+
+    # _logger.debug(f"{cmd = }")
+
+    # proc = subprocess.run(
+    #     [
+    #         *sudo_bash_c,
+    #         " ".join(install_service)
+    #     ],
+    #     shell=True,
+    #     check=True,
+    # )
+
+    _logger.info("Execute the following command manually:")
+    _logger.info(f"\n{' '.join(sudo_bash_c)} \"{' '.join(uninstall_service)}\"")
+
+    return cmd
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m OpenStudioLandscapesUtil.Harbor_CLI.skeleton 42
+    #
+    run()
